e6f6707f52b184492e80dadbc14ffd43338ca32f
naveenkumar sivarajan <sivarajan.naveenkumar@mahindra.com>
2022-08-24 15:45:33 +0530

    added azure terraform codebase for nic

--- variables.tf
+++ variables.tf
@@ -36,15 +36,15 @@ variable "vnet_name" {
 #   type       = string
 # }
 
-variable vms_configuration {
+variable "vms_configuration" {
   description = "the main input variable which has all the configuration regarding the vm to be created"
-  type        = list(object({
-    name               = string,
-    public_ip_enabled  = bool,
-    vnet_name          = string,
-    subnet_id          = string,
-    vm_size            = string,
-    storage_os_disk    = object({
+  type = list(object({
+    name              = string,
+    public_ip_enabled = bool,
+    vnet_name         = string,
+    subnet_id         = string,
+    vm_size           = string,
+    storage_os_disk = object({
       create_option             = string,
       caching                   = optional(string),
       disk_size_gb              = optional(number),
@@ -55,12 +55,12 @@ variable vms_configuration {
       managed_disk_type         = optional(string),
       vhd_uri                   = optional(string),
     }),
-    image_publisher    = string,
-    image_offer        = string,
-    image_sku          = string,
-    image_version      = string,
-    os_username        = string,
-    os_password        = string,
-    os_type            = string,
+    image_publisher = string,
+    image_offer     = string,
+    image_sku       = string,
+    image_version   = string,
+    os_username     = string,
+    os_password     = string,
+    os_type         = string,
   }))
 }
