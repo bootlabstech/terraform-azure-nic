4af1524414c248b46b02542adb56f5964a2c2dbb
naveenkumar sivarajan <sivarajan.naveenkumar@mahindra.com>
2022-08-24 22:04:19 +0530

    added azure terraform codebase for nic

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,6 @@ output "azurerm_virtual_network" {
   value = azurerm_virtual_network.vnet
 }
 
-output "azurerm_subnet" {
-  value = azurerm_subnet.subnet
-}
-
 output "azurerm_network_interface" {
   value = azurerm_network_interface.nic
 }
