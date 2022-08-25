5b667d6ae87e587499f4e24a983c5bf6eed57a82
naveenkumar sivarajan <sivarajan.naveenkumar@mahindra.com>
2022-08-25 22:07:43 +0530

    added azure terraform codebase for nic

--- main.tf
+++ main.tf
@@ -12,9 +12,10 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                = var.nic_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  name                      = var.nic_name
+  location                  = var.location
+  resource_group_name       = var.resource_group_name
+  network_security_group_id = azurerm_network_security_group.nsg.id
 
   ip_configuration {
     name                          = "internal"
